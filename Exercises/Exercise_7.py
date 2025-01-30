2f1405235c00262a447e148d0e0b4c7fc0b72991
Sreekar-Kandhadai <kandhadaisreekar@gmail.com>
2025-01-30 17:43:03 +0530

    Create Exercise_7.py

---
+++ Exercises/Exercise_7.py
@@ -0,0 +1,14 @@
+Write a program that will tell whether the given number is divisible by 3 & 6.
+
+Solution:
+
+num=6
+
+if num %6==0 and num %3==0:
+    
+    print(num,"is divisible by 3 and 6")
+    
+else:
+    
+    print(num,"is not  divisible by 3 and 6")
+    
