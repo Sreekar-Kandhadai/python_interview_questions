2abb657cdaa7ed94eba905284927b3a971294872
Sreekar-Kandhadai <kandhadaisreekar@gmail.com>
2025-01-30 15:31:39 +0530

    Update Exercise_2.py

--- Exercises/Exercise_2.py
+++ Exercises/Exercise_2.py
@@ -1,1 +1,10 @@
+Write a Program to extract each digit from an integer in the reverse order.
+For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
 
+Solution:
+
+number=73278
+
+for digit in  str(number)[::-1]:
+    
+    print(digit,end=" ")
